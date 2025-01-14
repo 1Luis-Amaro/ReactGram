e506ef1753c10a9b51f67eab1e3db449d9e51980
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-01-14 16:10:55 -0300

    Projeto ReactGram / 12 - Registro do usuário

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const  User = require("../models/User")
+const mongoose = require("mongoose")
 
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
@@ -13,12 +14,45 @@ const generateToken = (id) => {
 }
 
 // Register user and sign in
-const register = async(req, res) => {
-    res.send("Registro")
+const register = async (req, res) => {
+   
+   const {name, email, password} = req.body
+   
+   //check if user exists
+   const user = await User.findOne({email})
+
+   if(user) {
+    res.status(422).json({errors: ["Por favor, utilize outro e-mail"]})
+    return
+   }
+
+   //Generate password hash
+   const salt = await bcrypt.genSalt() //salt polui a string 
+   const passwordHash = await bcrypt.hash(password, salt)
+
+
+   //Create user
+   const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash
+   })
+
+   //If user was created sucessfull, return the token
+   if(!newUser) {
+    res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]})
+    return
 }
 
+   res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+   })
+
+}
 
 module.exports = {
     register,
     
 }
+
