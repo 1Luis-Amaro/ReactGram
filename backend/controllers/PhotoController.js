b29d42eac6abc10f555552fd980a9187fcd79b2f
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-01-24 10:04:49 -0300

    Projeto ReactGram / 30 - Funcionalidade de busca de fotos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -207,6 +207,15 @@ const commentPhoto = async (req, res) => {
   })
 };
 
+//Search photos by title
+const searchPhotos = async(req, res) => {
+    const {q} = req.query
+
+    const photos = await Photo.find({title: new RegExp(q, "i")}).exec()
+
+    res.status(200).json(photos)
+}
+
 module.exports = {
   insertPhoto,
   deletePhoto,
@@ -215,5 +224,6 @@ module.exports = {
   getPhotoById,
   updatePhoto,
   likePhoto,
-  commentPhoto
+  commentPhoto,
+  searchPhotos
 };
