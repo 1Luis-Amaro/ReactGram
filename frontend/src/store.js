5bda03b23fa8cafd4fca44507e78012c9c0346f5
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-02-19 17:23:00 -0300

    Projeto ReactGram / 72 - iniciando reducer de fotos

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";  // OK
 import userReducer from "./slices/userSlice";  // ✅ Importação correta
+import photoReducer from './slices/photoSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    photo: photoReducer
   },
 });
