faac4d66508b3440d85fdc634bb6505b5ac64106
Luis Otávio Miranda Amaro <luis.tavio03@gmail.com>
2025-02-25 14:58:51 -0300

    Projeto ReactGram / 89 - Exibindo imagem a dados

--- frontend/src/pages/Photo/Photo.js
+++ frontend/src/pages/Photo/Photo.js
@@ -9,12 +9,39 @@ import {useEffect, useState} from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
 
-//redux
 
+//redux
+import { getPhoto } from "../../slices/photoSlice"
+import PhotoItem from "../../components/PhotoItem"
 
 const Photo = () => {
-  return (
-    <div>Photo</div>
+  
+    const {id} = useParams()
+
+    const dispatch = useDispatch()
+
+    const {user} = useSelector((state) => state.auth)
+    const {photo, loading, error, message} = useSelector((state) => state.photo)
+
+    //comentarios
+
+    //load photo data
+    useEffect(() => {
+        dispatch(getPhoto(id))
+    }, [dispatch, id])
+
+
+    //like e comentario
+    if(loading) {
+        return <p>Carregando...</p>
+    }
+
+    return (
+    <div id="photo">
+        <PhotoItem photo={photo}/>
+        
+
+    </div>
   )
 }
 
